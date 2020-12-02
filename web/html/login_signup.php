e6f0206ecc5aa0000b0cfc8f1a8ac66789577fb2
”kensuke” <kensuke1202left@icloud.com>
2020-12-02 20:11:04 +0900

    signup機能の追加

--- web/html/login_signup.php
+++ web/html/login_signup.php
@@ -1,6 +1,7 @@
 <?php
 session_name("login");
 session_start();
+require_once "api.php";
 require_once "view/formhelper.php";
 require_once "database/connect.php";
 
@@ -9,7 +10,11 @@ if($_GET['info'] == "logout"){
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    list($errors, $input) = validate();
+    if($_POST['submit'] == "login"){
+        list($errors, $input) = login_validate();
+    }elseif($_POST['submit'] == "signup"){
+        list($errors, $input) = signup_validate();
+    }
     if($errors){
         show_form($errors);
     }else{
@@ -19,7 +24,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     show_form();
 }
 
-function validate(){
+function login_validate(){
     $input['name'] = trim($_POST['name'] ?? '');
     $input['password'] = trim($_POST['password'] ?? '');
 
@@ -27,15 +32,9 @@ function validate(){
         $stmt = $GLOBALS['db']->prepare('SELECT password FROM users WHERE name = ?');
         $stmt->execute((array)$input['name']);
         $row = $stmt->fetch();
-        
         if($row){
             //ここで本来はpassword_verifyを行う
-            // $password_ok = password_verify($input['password'], $row);
-            if($row['password'] === $input['password']){
-                $password_ok = true;
-            }else{
-                $password_ok = false;
-            }
+            $password_ok = password_verify($input['password'], $row["password"]);
         }
         if(!$password_ok){
             $errors[] = 'Please enter a valid username and password';
@@ -44,6 +43,19 @@ function validate(){
         $errors[] = 'Please enter a valid username and password';
     }
     
+    return array($errors, $input);
+}
+
+function signup_validate(){
+    $input['name'] = trim($_POST['name'] ?? '');
+    $input['password'] = trim($_POST['password'] ?? '');
+
+    if(empty($input['name']) || !preg_match("/^[a-zA-Z0-9]+$/", $input['name'])){
+        $errors[] = 'Please enter your name';
+    }
+    if(empty($input['password']) || !preg_match("/^[a-zA-Z0-9]+$/", $input['password'])){
+        $errors[] = 'Please enter your password';
+    }
     return array($errors, $input);
 }
 
@@ -56,10 +68,17 @@ function show_form($errors = array()){
 
 function process_form($input){
     
-    //sessonにusernameをセットしてログイン状態にする
-    $_SESSION['username'] = $input['name'];
+    if($_POST['submit'] == "login"){
+        //sessonにusernameをセットしてログイン状態にする
+        $_SESSION['username'] = $input['name'];
+    }elseif($_POST['submit'] == "signup"){
+        //nameとpasswordをdbにセットする
+        $hashedpassword = password_hash($input['password'], PASSWORD_DEFAULT);
+        insert_user($input['name'], $hashedpassword);
+    }
 
     $info = $_GET['info'] ?? 'login';
+    $api = new Api;
     $form = new FormHelper;
     include 'main_index.php';
 }
